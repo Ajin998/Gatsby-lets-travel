774ffe3bc04ad01562b8a957b24a9591442d7855
Ajin Varghese <ajin.varghese@contentstack.com>
2021-02-01 19:40:33 +0530

    Added Footer components in about and contact us pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
-
+import Footer from "../components/footer"
 const About = () => {
   return (
     <div>
       <Header />
       <h1>This is About page</h1>
+    <Footer/>
     </div>
   );
 };
--- src/pages/contactUs.js
+++ src/pages/contactUs.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/footer";
 const contactUs = () => {
   return (
     <div>
       <Header />
       <h1>This is Contact us Page</h1>
+      <Footer/>
     </div>
   );
 };
